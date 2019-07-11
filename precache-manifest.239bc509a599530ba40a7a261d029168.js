fef28335028f83c11479daf55ec35be648050d4f
skravche <sktechno92@gmail.com>
2019-07-11 19:14:59 +0300

    Updates

---
+++ precache-manifest.239bc509a599530ba40a7a261d029168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "418b65f979098f059df0",
+    "url": "/phone-catalog-react-redux/static/js/runtime~main.418b65f9.js"
+  },
+  {
+    "revision": "008ad2cc88190937dddf",
+    "url": "/phone-catalog-react-redux/static/js/main.008ad2cc.chunk.js"
+  },
+  {
+    "revision": "4caf6c61e52425fc5fea",
+    "url": "/phone-catalog-react-redux/static/js/2.4caf6c61.chunk.js"
+  },
+  {
+    "revision": "008ad2cc88190937dddf",
+    "url": "/phone-catalog-react-redux/static/css/main.5f15480b.chunk.css"
+  },
+  {
+    "revision": "f6238a38ddff762f8a18a1b1f1cee7b2",
+    "url": "/phone-catalog-react-redux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/phone-catalog-react-redux/precache-manifest.239bc509a599530ba40a7a261d029168.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/phone-catalog-react-redux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
